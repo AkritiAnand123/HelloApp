94f48f4ca15fe6148d845a3d0788933f8b4b5c41
Akriti <akritianand2006@gmail.com>
2026-03-18 10:48:12 +0530

    UC1

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// package main.java;
+
+// public class HelloApp {
+    
+// }
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
